8f1fcb1a3999fcca03c488a0cd40f95fb872b5ab
Yerkebulan <59691878+Yerkebulanskii@users.noreply.github.com>
2020-11-18 00:21:27 +0600

    second

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -122,9 +122,9 @@ STATIC_URL = '/static/'
 
 
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+#STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, 'static')
+#]
 
 MEDIA_URL = '/images/'
 
